775c898c9722612caee44900838d016a1348bde1
Steve Bosilovatz <steve@bosilovatz.com>
2025-02-15 19:20:33 -0600

    Fix test

--- DbLocatorTests/DatabaseServerTests.cs
+++ DbLocatorTests/DatabaseServerTests.cs
@@ -13,7 +13,7 @@ public class DatabaseServerTests(DbLocatorFixture dbLocatorFixture)
     public async Task AddMultipleDatabaseServersAndSearchByKeyWord()
     {
         var databaseServerName = StringUtilities.RandomString(10);
-        await _dbLocator.AddDatabaseServer(databaseServerName, "127.0.0.1");
+        await _dbLocator.AddDatabaseServer(databaseServerName, "192.168.1.1");
 
         var databaseServers = (await _dbLocator.GetDatabaseServers())
             .Where(x => x.Name == databaseServerName)
@@ -21,14 +21,17 @@ public class DatabaseServerTests(DbLocatorFixture dbLocatorFixture)
 
         Assert.Single(databaseServers);
         Assert.Equal(databaseServerName, databaseServers[0].Name);
-        Assert.Equal("127.0.0.1", databaseServers[0].IpAddress);
+        Assert.Equal("192.168.1.1", databaseServers[0].IpAddress);
     }
 
     [Fact]
     public async Task AddAndDeleteDatabaseServer()
     {
         var databaseServerName = StringUtilities.RandomString(10);
-        var databaseServerId = await _dbLocator.AddDatabaseServer(databaseServerName, "127.0.0.1");
+        var databaseServerId = await _dbLocator.AddDatabaseServer(
+            databaseServerName,
+            "192.168.1.1"
+        );
 
         await _dbLocator.DeleteDatabaseServer(databaseServerId);
         var databaseServers = (await _dbLocator.GetDatabaseServers())
