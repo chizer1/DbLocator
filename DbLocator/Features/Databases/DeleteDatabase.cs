88ab6eba55a405db6632a71695880f0f47a779ac
parkerstovall <parker.stovall@gmail.com>
2025-04-12 15:08:17 -0500

    removed method to test action

--- DbLocator/Features/Databases/DeleteDatabase.cs
+++ DbLocator/Features/Databases/DeleteDatabase.cs
@@ -46,40 +46,43 @@ namespace DbLocator.Features.Databases
 
             if (command.DeleteDatabase == true)
             {
-                await DeleteDatabaseAsync(dbContext, databaseEntity);
+                //await DeleteDatabaseAsync(dbContext, databaseEntity);
             }
 
             cache?.Remove("databases");
+
+            // TODO: Make this more specific
+            cache?.TryClearConnectionStringFromCache(DatabaseTypeId: databaseEntity.DatabaseTypeId);
         }
 
-        private static async Task DeleteDatabaseAsync(
-            DbLocatorContext dbContext,
-            DatabaseEntity databaseEntity
-        )
-        {
-            var databaseName = Sql.SanitizeSqlIdentifier(databaseEntity.DatabaseName);
-            var rawCommand = $"drop database [{databaseName}]";
+        // private static async Task DeleteDatabaseAsync(
+        //     DbLocatorContext dbContext,
+        //     DatabaseEntity databaseEntity
+        // )
+        // {
+        //     var databaseName = Sql.SanitizeSqlIdentifier(databaseEntity.DatabaseName);
+        //     var rawCommand = $"drop database [{databaseName}]";
 
-            var databaseServer = await dbContext
-                .Set<DatabaseServerEntity>()
-                .FirstOrDefaultAsync(ds => ds.DatabaseServerId == databaseEntity.DatabaseServerId);
+        //     var databaseServer = await dbContext
+        //         .Set<DatabaseServerEntity>()
+        //         .FirstOrDefaultAsync(ds => ds.DatabaseServerId == databaseEntity.DatabaseServerId);
 
-            string commandText;
-            if (databaseServer?.IsLinkedServer == true)
-            {
-                var linkedServer = Sql.SanitizeSqlIdentifier(databaseServer.DatabaseServerHostName);
-                var escapedCommand = Sql.EscapeForDynamicSql(rawCommand);
-                commandText = $"exec('{escapedCommand}') at [{linkedServer}];";
-            }
-            else
-            {
-                commandText = rawCommand;
-            }
+        //     string commandText;
+        //     if (databaseServer?.IsLinkedServer == true)
+        //     {
+        //         var linkedServer = Sql.SanitizeSqlIdentifier(databaseServer.DatabaseServerHostName);
+        //         var escapedCommand = Sql.EscapeForDynamicSql(rawCommand);
+        //         commandText = $"exec('{escapedCommand}') at [{linkedServer}];";
+        //     }
+        //     else
+        //     {
+        //         commandText = rawCommand;
+        //     }
 
-            using var cmd = dbContext.Database.GetDbConnection().CreateCommand();
-            cmd.CommandText = commandText;
-            await dbContext.Database.OpenConnectionAsync();
-            await cmd.ExecuteNonQueryAsync();
-        }
+        //     using var cmd = dbContext.Database.GetDbConnection().CreateCommand();
+        //     cmd.CommandText = commandText;
+        //     await dbContext.Database.OpenConnectionAsync();
+        //     await cmd.ExecuteNonQueryAsync();
+        // }
     }
 }
